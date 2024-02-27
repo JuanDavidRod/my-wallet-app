0315574557faa6d37403988485bc3eacfb5e1b10
Juan David Rodriguez <juandavid.rodriguez0222@gmail.com>
2024-02-26 19:26:00 -0500

    feat: add component header

---
+++ app/ui/dashboard/header.tsx
@@ -0,0 +1,11 @@
+const header = () => {
+  return (
+    <header className="mx-5 mb-2 mt-3 flex">
+      <h1 className="bg-gradient-to-r from-[#f562e4] from-0% via-[#8c63ec] via-35% to-[#3763f2] to-100% bg-clip-text text-center text-5xl font-bold text-transparent md:w-56">
+        My Wallet
+      </h1>
+    </header>
+  )
+}
+
+export default header

fbd8d2311d542fde7ff423155fe84a0b6d57086a
Juan David Rodriguez <juandavid.rodriguez0222@gmail.com>
2024-02-26 19:08:19 -0500

    feat: add Rubik font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Rubik } from 'next/font/google'
 import './globals.css'
-const inter = Inter({ subsets: ['latin'] })
+const inter = Rubik({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',

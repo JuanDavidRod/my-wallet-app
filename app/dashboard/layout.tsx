dcda8106efb16d38fd334c570824eeba376ddbec
Juan David Rodriguez <juandavid.rodriguez0222@gmail.com>
2024-02-26 19:06:36 -0500

    feat: finish header to layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,14 +2,19 @@ import SideNav from '../ui/dashboard/sidenav'
 
 const layuout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
+    <>
+      <header className="mx-5 mt-6 flex">
+        <h1 className="bg-gradient-to-r from-[#f562e4] from-0% via-[#8c63ec] via-35% to-[#3763f2] to-100% bg-clip-text text-center text-4xl font-bold text-transparent md:w-56">
+          My Wallet
+        </h1>
+      </header>
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
         <SideNav />
+        <main className="h-[90vh] flex-grow rounded-[20px] bg-[#2C2C2C] p-6 md:overflow-y-auto md:p-12">
+          {children}
+        </main>
       </div>
-      <div className="flex-grow bg-[#2C2C2C] border-r-1 p-6 md:overflow-y-auto md:p-12">
-        {children}
-      </div>
-    </div>
+    </>
   )
 }
 
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,7 +2,7 @@ import NavLinks from './navl-inks'
 
 const sideNav = () => {
   return (
-    <nav className="flex h-full flex-col px-2 py-4 md:px-4">
+    <nav className="flex h-full w-full flex-col px-2 py-4 md:w-64 md:px-4">
       <NavLinks />
     </nav>
   )
